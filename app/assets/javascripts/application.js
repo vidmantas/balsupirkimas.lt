bfa71e6880a949541bc4f620b5c49aa00a213536
Adomas Sliuzinskas <adomas@samesystem.dk>
2012-10-19 16:46:22 +0300

    Added party list to autosuggest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,28 @@ $(document).ready(function() {
       "Naujamiesčio","Senamiesčio","Antakalnio","Žirmūnų","Fabijoniškių","Šeškinės","Justiniškių","Karoliniškių","Lazdynų","Naujosios Vilnios","Šilainių","Aleksoto -  Vilijampolės","Centro","Žaliakalnio","Kalniečių","Dainavos","Pramonės","Panemunės","Danės","Baltijos","Marių","Pajūrio","Aušros","Saulės","Dainų","Nevėžio","Vakarinė","Aukštaitijos","Marijampolės","Alytaus","Gargždų","Šilutės - Pagėgių","Šilalės - Šilutės","Tauragės","Plungės - Rietavo","Kretingos","Skuodo  - Mažeikių","Mažeikių","Akmenės - Joniškio","Telšių","Kelmės","Raseinių","Kėdainių","Radviliškio","Šiaulių kaimiškoji","Pakruojo - Joniškio","Pasvalio - Panevėžio","Biržų - Kupiškio","Anykščių - Kupiškio","Rokiškio","Utenos","Zarasų - Visagino","Ignalinos - Švenčionių","Molėtų - Švenčionių","Širvintų - Vilniaus","Vilniaus - Šalčininkų","Vilniaus - Trakų","Trakų - Elektrėnų","Kaišiadorių - Elektrėnų","Jonavos","Ukmergės","Jurbarko","Suvalkijos","Šakių","Kauno - Kėdainių","Kauno kaimiškoji","Prienų - Birštono","Vilkaviškio","Dzūkijos","Varėnos - Eišiškių","Lazdijų - Druskininkų"
     ]
   });
+  $("#spot_party").autocomplete({
+    source: [
+      "1. Liberalų sąjūdis",
+      "2. Respublikonų partija",
+      "3. Darbo partija",
+      "4. Demokratinė darbo ir vienybės partija",
+      "5. Tėvynės sąjunga-Lietuvos krikščionys demokratai",
+      "6. \"Drąsos kelias\"",
+      "7. Lietuvos lenkų rinkimų akcija",
+      "8. Lietuvos socialdemokratų partija", 
+      "9. \"Tvarka ir teisingumas\"", 
+      "10. Nacionalinis susivienijimas \"Už Lietuvą Lietuvoje\"", 
+      "11. Krikščionių partija", 
+      "12. Lietuvos žmonių partija", 
+      "13. Socialistinis liaudies frontas", 
+      "14. Lietuvos valstiečių ir žaliųjų sąjunga", 
+      "15. \"Jaunoji Lietuva\"", 
+      "16. Liberalų ir centro sąjunga", 
+      "17. Emigrantų partija", 
+      "18. Politinė partija \"Sąjunga TAIP\""
+    ]
+  })
 });
 
 var _gaq = _gaq || [];
